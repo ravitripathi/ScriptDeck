aee3d601832ae7b001b2ba724661722ffa5ea673
Ravi Tripathi <ravi.tripathi@go-jek.com>
2020-05-31 16:13:38 +0530

    Initial Commit

---
+++ StatusScript/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  StatusScript
+//
+//  Created by Ravi Tripathi on 31/05/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StatusScript/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StatusScript
+//
+//  Created by Ravi Tripathi on 31/05/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
